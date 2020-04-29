7b2aa36364ebe6b76dcba32b3333590c374cabd1
Bernardo <bernaferrari2@gmail.com>
2020-04-29 14:57:51 -0300

    update flutter. Use SKIA by default.

--- fastdarktheme/flutter_service_worker.js
+++ fastdarktheme/flutter_service_worker.js
@@ -1,17 +1,18 @@
 'use strict';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "/index.html": "2fb3749791b10c3c8b7e4e7bc921f50b",
-"/main.dart.js": "c011832f816d714c610ad25a6d53d887",
-"/favicon.png": "4c24c0cb9ff4c1b19e5715b2ef672fb4",
-"/icons/Icon-192.png": "c245c8ef7d5c1d92c3f33d121dd16776",
-"/icons/Icon-512.png": "f23f03f8c5fa298d006028e4cc7fe803",
-"/manifest.json": "d475bff0a03e56d733e7dd675e551368",
-"/assets/LICENSE": "b0513b62240548a6f242d0eaddb5e69f",
-"/assets/AssetManifest.json": "d42c612ee655100a095de4a54a5523e1",
-"/assets/FontManifest.json": "8a636fd7e85453a42a4ef7cedaf53812",
-"/assets/packages/flutter_feather_icons/fonts/feather.ttf": "c96dc22ca29a082af83cce866d35cebc",
-"/assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16"
+  "index.html": "5f70a9060418ac1cf47c2d0652dc04d8",
+"/": "5f70a9060418ac1cf47c2d0652dc04d8",
+"main.dart.js": "8ec1945fbce69e02a66bf4e3a342be57",
+"favicon.png": "4c24c0cb9ff4c1b19e5715b2ef672fb4",
+"icons/Icon-192.png": "c245c8ef7d5c1d92c3f33d121dd16776",
+"icons/Icon-512.png": "f23f03f8c5fa298d006028e4cc7fe803",
+"manifest.json": "d475bff0a03e56d733e7dd675e551368",
+"assets/LICENSE": "4a76a2b91f3a2d23d166d0b617637448",
+"assets/AssetManifest.json": "d42c612ee655100a095de4a54a5523e1",
+"assets/FontManifest.json": "8a636fd7e85453a42a4ef7cedaf53812",
+"assets/packages/flutter_feather_icons/fonts/feather.ttf": "c96dc22ca29a082af83cce866d35cebc",
+"assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16"
 };
 
 self.addEventListener('activate', function (event) {
