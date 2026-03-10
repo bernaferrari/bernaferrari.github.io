3feef9baf1e58087a4deb045637fc7f029eb11a2
Bernardo Ferrari <bernaferrari2@gmail.com>
2026-03-10 16:42:39 -0300

    flutter diagonal wipe icon

--- diagonal-wipe-icon-flutter/flutter_bootstrap.js
+++ diagonal-wipe-icon-flutter/flutter_bootstrap.js
@@ -33,10 +33,10 @@ addEventListener("message", eventListener);
 if (!window._flutter) {
   window._flutter = {};
 }
-_flutter.buildConfig = {"engineRevision":"d802cab600fc03a2abd4fd4aecf147d62dd814da","builds":[{"compileTarget":"dart2wasm","renderer":"skwasm","mainWasmPath":"main.dart.wasm","jsSupportRuntimePath":"main.dart.mjs"},{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.dart.js"}]};
+_flutter.buildConfig = {"engineRevision":"195ae7b3a1220a0eaf0525637b0249c5b624af94","builds":[{"compileTarget":"dart2wasm","renderer":"skwasm","mainWasmPath":"main.dart.wasm","jsSupportRuntimePath":"main.dart.mjs"},{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.dart.js"}]};
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3512824936" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3369640304" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
